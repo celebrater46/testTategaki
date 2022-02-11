56046e9d12a615eec3e0c5d38608c59a3108276b
celebrater <tkgm45nbchk@gmail.com>
2022-02-12 03:43:48 +0900

    displayed as writing-mode vertical-rl successfully

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 "use strict";
 
-const box = document.getElementById("box");
+const testLine = "「これは……テストでーす」";
+const div = document.getElementById("p-1");
+
+const rotateChar = (line) => {
+    let str = line;
+    // str = str.replace(/「/g, "<span class='rotate'>「</span>");
+    // str = str.replace(/」/g, "<span class='rotate'>」</span>");
+    // str = str.replace(/ー/g, "<span class='rotate'>ー</span>");
+    return str;
+}
+
+const createLines = (line) => {
+    let str = "";
+    for (let i = 0; i < 15; i++){
+        const rotated = rotateChar(line);
+        str += "<p id='line-" + i + "'>" + rotated + "</p>";
+    }
+    return str;
+}
+
+div.innerHTML = createLines(testLine);
