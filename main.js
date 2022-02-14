190f48db1d5fe921ef18938a8f4027afe1eed11b
celebrater <tkgm45nbchk@gmail.com>
2022-02-15 02:49:09 +0900

    tried other Google Fonts as Tategaki. It does not need to add <span> and to adjust layout with CSS

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 "use strict";
 
-const testLine = "「これは……テストでーす――」";
+const testLine = "「これは……《テスト》でーす――。〈テスト〉【テスト】だよ、ね？　そう!!」";
 const div = document.getElementById("p-1");
 
-const rotateChar = (line) => {
+const addSpanToChar = (line) => {
     let str = line;
-    // str = str.replace(/「/g, "<span class='rotate'>「</span>");
-    // str = str.replace(/」/g, "<span class='rotate'>」</span>");
-    // str = str.replace(/ー/g, "<span class='rotate'>ー</span>");
+    // str = str.replace(/「/g, "<span class='startBracket'>「</span>");
+    // str = str.replace(/…/g, "<span class='leader'>…</span>");
+    // str = str.replace(/―/g, "<span class='dash'>―</span>");
+    // str = str.replace(/ー/g, "<span class='hyphen'>ー</span>");
+    // str = str.replace(/」/g, "<span class='endBracket'>」</span>");
     return str;
 }
 
 const createLines = (line) => {
     let str = "";
     for (let i = 0; i < 15; i++){
-        const rotated = rotateChar(line);
+        const rotated = addSpanToChar(line);
         str += "<p id='line-" + i + "'>" + rotated + "</p>";
     }
     return str;
